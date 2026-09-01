841575cfb9bf3d2ec9fdb88a38dfd3ae04dd870f
Jahnavi Tanarapu <jahnavitanarapu@gmail.com>
2026-09-01 10:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (36.67%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,20 @@
-'''optimal'''       
+'''brute force'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(1,len(nums)):
+                target==nums[i]+nums[j]
+        return(i,j)        
+
+
+'''optimal solution'''
 class Solution(object):
     def twoSum(self, nums, target):
         dict={}
         for i in range(len(nums)):
-            current=target-nums[i]
-            if current in dict:
-                return(dict[current],i)
-            dict[nums[i]]=i
+            current_element=target-nums[i]
+            if current_element in dict:
+                return(dict[current_element],i)
+            dict[nums[i]]=i    
+
+
